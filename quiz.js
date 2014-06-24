eec1cd07059b627628494a1e874ef9e739410197
amykirst <amylkirst@gmail.com>
2014-06-23 22:47:42 -0400

    Updated JS

--- quiz.js
+++ quiz.js
@@ -14,28 +14,28 @@ var questions = [{
   qText: "A gold and turquoise ring once owned by Jane Austen was purchased by which U.S. singer?",
   qChoices: ["Taylor Swift", "Kelly Clarkson", "Gwen Stefani", "Sara Bareilles"],
   qAnswer: "Kelly Clarkson",
-  qDescription: "Kelly Clarkson. The singer purchased the ring at auction last year. However, Clarkson was unable to take the ring out of the country, as UK Culture Minister Ed Vaizey banned the object from leaving the country, citing it as a national treasure. Clarkson agreed to re-sell the ring when The Jane Austen House Museum raised the money needed to match Clarkson's bid. The ring had been in Austen's family for 200 years."
+  qDescription: "Kelly Clarkson purchased Jane Auasten's gold and turquoise ring at auction last year. However, Clarkson was unable to take the ring out of the country, as UK Culture Minister Ed Vaizey banned the object from leaving the country, citing it as a national treasure. Clarkson agreed to re-sell the ring when The Jane Austen House Museum raised the money needed to match Clarkson's bid. The ring had been in Austen's family for 200 years."
 }, {
   qName: "child",
   qNum: 3,
   qText: 'Which of her books did Jane Austen call "my darling child"?',
   qChoices: ["Pride and Prejudice", "Emma", "Mansfield Park", "Sense and Sensibility"],
   qAnswer: "Pride and Prejudice",
-  qDescription: 'Pride and Prejudice. In a letter to her sister Cassandra, dated January 29, 1813, Jane spoke of receiving her copy of the three-volume book from her publisher on 27 January. She said, “I want to tell you I have got my own darling child from London."'
+  qDescription: 'Jane referred to Pride and Prejudice as "her darling child." In a letter to her sister Cassandra, dated January 29, 1813, Jane spoke of receiving her copy of the three-volume book from her publisher. She said, “I want to tell you I have got my own darling child from London."'
 }, {
   qName: "marry",
   qNum: 4,
   qText: "Who did Jane agree to marry but changed her mind the next day?",
   qChoices: ["Tom Lefroy", "Harris Bigg-Wither", "A clergyman, whose name is unknown", "Thomas Fowle"],
   qAnswer: "Harris Bigg-Wither",
-  qDescription: 'Harris Bigg-Wither, a wealthy neighbor. Jane later wrote to a family member, "Anything is to be preferred and endured rather than marrying without affection."'
+  qDescription: 'Jane agreed to marry Harris Bigg-Wither, a wealthy neighbor. She later wrote to a family member, "Anything is to be preferred and endured rather than marrying without affection."'
 }, {
   qName: "death",
   qNum: 5,
   qText: "How old was Jane Austen when she died?",
   qChoices: [37, 42, 45, 50],
   qAnswer: 42,
-  qDescription: "Jane Austen was 42 when she died. It was thought that the author died of Addison's disease, a rare hormonal disorder. However, it is thought that Jane was too lucid in her final days to have died of Addison's disease. It has been suggested that Austen died of tuberculosis after drinking unpasteurized milk.",
+  qDescription: "Jane Austen was 42 when she died. It was thought that the author died of Addison's disease, a rare hormonal disorder. However, because she was too lucid in her final days to have died of Addison's disease, it has been suggested that Austen died of tuberculosis after drinking unpasteurized milk.",
 }]
 // questions testing
 // console.log(questions.length); // equals 5
@@ -46,12 +46,14 @@ var questions = [{
 // console.log('<input name="' + questions[currentQuestion].qName + '" ' + 'type="radio" value="' + questions[currentQuestion].qChoices[0] + '">' + questions[currentQuestion].qChoices[0] + '<br>');
 // test the number of questions in array
 //console.log(questions[currentQuestion].qChoices.length);
+// create input tags for radio buttons
 
 function createInputTag(currentQuestion) {
   var thisAnswer = '<input name="' + questions[currentQuestion].qName + '" ' + 'type="radio" value="' + questions[currentQuestion].qChoices[counter] + '">' + questions[currentQuestion].qChoices[counter] + '<br>';
   return thisAnswer;
 }
 
+// display question
 function showQuestion() {
   var radio = [];
   // create inputs for radio buttons
@@ -70,43 +72,54 @@ function showQuestion() {
   $(".options").append('<input name="submit" type="submit" value="Submit">');
 }
 
+// grade question
 function gradeQuestion() {
   // get the value of the checked radio button:
   var answerValue = $("input[type='radio']:checked").val();
-  // test value of checked button - comes back as undefined.
+  // test value of checked button
   // console.log(answerValue);
-  // hide previous question number
-  $(".questionNum").empty();
-  // hide question text within .questions
-  $(".question").empty();
-  // hide radio buttons 
-  $(".options").empty();
-  // hide submit button
-  $(".options").empty();
+  // hide previous question 
+  clearPrevious();
   // compare selected option to answer
   // test value of current question answer
   // console.log(questions[currentQuestion].qAnswer);
-  if (answerValue == questions[currentQuestion].qAnswer) {
-    // display "correct" message
-    $(".question").append("<p>" + "Correct!" + "</p>");
-    // add to the score
-    score++;
-    // test score
-    // console.log(score);
+  if (answerValue == null) {
+    alert("Please select an answer.");
+    showQuestion();
   }
   else {
-    // display "incorrect" message
-    $(".question").append("<p>" + "Sorry, that is incorrect." + "</p>");
+  	// show if answer was correct
+  	if (answerValue == questions[currentQuestion].qAnswer) {
+      // display "correct" message
+      $(".question").append("<p>" + "Correct!" + "</p>");
+      // add to the score
+      score++;
+      // test score
+      // console.log(score);
+    }
+    else {
+      // display "incorrect" message
+      $(".question").append("<p>" + "Sorry, that is incorrect." + "</p>");
+    }
+    // display explanation of correct answer
+    $(".question").append(questions[currentQuestion].qDescription);
+    // display next question button or score button
+    if (questions[currentQuestion].qNum == 5) {
+      $(".options").append('<input name="score" type="submit" value="Show Score">');
+    }
+    else {
+      $(".options").append('<input name="next" type="submit" value="Next Question">');
+    }
   }
-  // display explanation of correct answer
-  $(".question").append(questions[currentQuestion].qDescription);
-  // display next question button or score button
-  if (questions[currentQuestion].qNum == 5) {
-    $(".options").append('<input name="score" type="submit" value="Show Score">');
-  }
-  else {
-    $(".options").append('<input name="next" type="submit" value="Next Question">');
-  }
+}
+
+// clear previous question displays
+function clearPrevious() {
+  $(".questionNum").empty();
+  // hide question text within .questions
+  $(".question").empty();
+  // hide radio and submit buttons 
+  $(".options").empty();
 }
 
 
@@ -115,26 +128,39 @@ $(document).ready(function() {
   showQuestion();
 
   // When user clicks submit, grade the question
-  $("input[name='submit']").click(function() {
-    //alert("I'm grading the test!");
+  $(document).on("click", "input[name='submit']", function() {
+    //alert("line 118: I clicked on submit");
     gradeQuestion();
   });
 
   // When user clicks "Next Question", show next question
-  $("input[name='next']").click(function() {
-    alert("I clicked on next");
+  $(document).on("click", "input[name='next']", function() {
     // update currentQuestion to next question
     currentQuestion++;
+    // hide previous question 
+    clearPrevious();
     //show question
     showQuestion();
+  });
+
+  // When user clicks on "Show Score", show score
+  $(document).on("click", "input[name='score']", function() {
+    // hide previous question 
+    clearPrevious();
+    // display score
+    $(".question").append("<p>" + "You got " + score + " out of " + questions.length + " questions correct.");
+    // play again
+    $(".options").append('<input name="play" type="submit" value="Play Again">');
+  });
+
+  // When user clicks to play again, refresh page
+  $(document).on("click", "input[name='play']", function() {
+    location.reload();
   });
 
   // Prevent form from refreshing page
   $("form").submit(function(e) {
     e.preventDefault();
   });
-  
-  // If "show score" button is clicked, show overall score (show score variable)
-  // Need to prevent from being able to submit without an answer
 
 }); // end document ready
