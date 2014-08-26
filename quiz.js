39a02800bc0b81645e9f3f9c0dbd1d01752f58d0
Amy Kirst <amylkirst@gmail.com>
2014-08-25 20:54:05 -0400

    Updated object names

--- quiz.js
+++ quiz.js
@@ -1,55 +1,56 @@
+var questions = [{
+  name: "published",
+  num: 1,
+  text: "How many of Jane Austen's books were published in her lifetime?",
+  choices: [3, 4, 5, 6],
+  answer: 4,
+  description: "Four of Jane Austen's six novels were published in her lifetime: Sense & Sensibility, Pride & Prejudice, Mansfield Park, and Emma. Persuasion and Northanger Abbey were published after her death. Their publication was arranged by her sister Cassandra and her brother Henry Austen."
+}, {
+  name: "ring",
+  num: 2,
+  text: "A gold and turquoise ring once owned by Jane Austen was purchased by which U.S. singer?",
+  choices: ["Taylor Swift", "Kelly Clarkson", "Gwen Stefani", "Sara Bareilles"],
+  answer: "Kelly Clarkson",
+  description: "Kelly Clarkson purchased Jane Auasten's gold and turquoise ring at auction last year. However, Clarkson was unable to take the ring out of the country, as UK Culture Minister Ed Vaizey banned the object from leaving the country, citing it as a national treasure. Clarkson agreed to re-sell the ring when The Jane Austen House Museum raised the money needed to match Clarkson's bid. The ring had been in Austen's family for 200 years."
+}, {
+  name: "child",
+  num: 3,
+  text: 'Which of her books did Jane Austen call "my darling child"?',
+  choices: ["Pride and Prejudice", "Emma", "Mansfield Park", "Sense and Sensibility"],
+  answer: "Pride and Prejudice",
+  description: 'Jane referred to Pride and Prejudice as "her darling child." In a letter to her sister Cassandra, dated January 29, 1813, Jane spoke of receiving her copy of the three-volume book from her publisher. She said, “I want to tell you I have got my own darling child from London."'
+}, {
+  name: "marry",
+  num: 4,
+  text: "Who did Jane agree to marry but changed her mind the next day?",
+  choices: ["Tom Lefroy", "Harris Bigg-Wither", "A clergyman, whose name is unknown", "Thomas Fowle"],
+  answer: "Harris Bigg-Wither",
+  description: 'Jane agreed to marry Harris Bigg-Wither, a wealthy neighbor. She later wrote to a family member, "Anything is to be preferred and endured rather than marrying without affection."'
+}, {
+  name: "death",
+  num: 5,
+  text: "How old was Jane Austen when she died?",
+  choices: [37, 42, 45, 50],
+  answer: 42,
+  description: "Jane Austen was 42 when she died. It was thought that the author died of Addison's disease, a rare hormonal disorder. However, because she was too lucid in her final days to have died of Addison's disease, it has been suggested that Austen died of tuberculosis after drinking unpasteurized milk.",
+}];
 var score = 0;
 var counter = 0;
+// Counter for question
 var currentQuestion = 0;
-var questions = [{
-  qName: "published",
-  qNum: 1,
-  qText: "How many of Jane Austen's books were published in her lifetime?",
-  qChoices: [3, 4, 5, 6],
-  qAnswer: 4,
-  qDescription: "Four of Jane Austen's six novels were published in her lifetime: Sense & Sensibility, Pride & Prejudice, Mansfield Park, and Emma. Persuasion and Northanger Abbey were published after her death. Their publication was arranged by her sister Cassandra and her brother Henry Austen."
-}, {
-  qName: "ring",
-  qNum: 2,
-  qText: "A gold and turquoise ring once owned by Jane Austen was purchased by which U.S. singer?",
-  qChoices: ["Taylor Swift", "Kelly Clarkson", "Gwen Stefani", "Sara Bareilles"],
-  qAnswer: "Kelly Clarkson",
-  qDescription: "Kelly Clarkson purchased Jane Auasten's gold and turquoise ring at auction last year. However, Clarkson was unable to take the ring out of the country, as UK Culture Minister Ed Vaizey banned the object from leaving the country, citing it as a national treasure. Clarkson agreed to re-sell the ring when The Jane Austen House Museum raised the money needed to match Clarkson's bid. The ring had been in Austen's family for 200 years."
-}, {
-  qName: "child",
-  qNum: 3,
-  qText: 'Which of her books did Jane Austen call "my darling child"?',
-  qChoices: ["Pride and Prejudice", "Emma", "Mansfield Park", "Sense and Sensibility"],
-  qAnswer: "Pride and Prejudice",
-  qDescription: 'Jane referred to Pride and Prejudice as "her darling child." In a letter to her sister Cassandra, dated January 29, 1813, Jane spoke of receiving her copy of the three-volume book from her publisher. She said, “I want to tell you I have got my own darling child from London."'
-}, {
-  qName: "marry",
-  qNum: 4,
-  qText: "Who did Jane agree to marry but changed her mind the next day?",
-  qChoices: ["Tom Lefroy", "Harris Bigg-Wither", "A clergyman, whose name is unknown", "Thomas Fowle"],
-  qAnswer: "Harris Bigg-Wither",
-  qDescription: 'Jane agreed to marry Harris Bigg-Wither, a wealthy neighbor. She later wrote to a family member, "Anything is to be preferred and endured rather than marrying without affection."'
-}, {
-  qName: "death",
-  qNum: 5,
-  qText: "How old was Jane Austen when she died?",
-  qChoices: [37, 42, 45, 50],
-  qAnswer: 42,
-  qDescription: "Jane Austen was 42 when she died. It was thought that the author died of Addison's disease, a rare hormonal disorder. However, because she was too lucid in her final days to have died of Addison's disease, it has been suggested that Austen died of tuberculosis after drinking unpasteurized milk.",
-}]
-// questions testing
-// console.log(questions.length); // equals 5
-// console.log(questions[0].qNum); // equals 1
-// console.log(questions[0].qText) // equals "How many of Jane Austen's books were published in her lifetime?"
-// console.log(typeof questions); // equals object
-// Create radio buttons
-// console.log('<input name="' + questions[currentQuestion].qName + '" ' + 'type="radio" value="' + questions[currentQuestion].qChoices[0] + '">' + questions[currentQuestion].qChoices[0] + '<br>');
-// test the number of questions in array
-//console.log(questions[currentQuestion].qChoices.length);
-// create input tags for radio buttons
+// Store active question in question variable
+var question = questions[currentQuestion];
+// Text for current question 
+var currentText = question.text; 
 
+
+// Create radio buttons
+// console.log('<input name="' + question.name + '" ' + 'type="radio" value="' + question.choices[0] + '">' + question.choices[0] + '<br>');
+
+
+// create input tags for radio buttons
 function createInputTag(currentQuestion) {
-  var thisAnswer = '<input name="' + questions[currentQuestion].qName + '" ' + 'type="radio" value="' + questions[currentQuestion].qChoices[counter] + '">' + questions[currentQuestion].qChoices[counter] + '<br>';
+  var thisAnswer = '<input name="' + question.name + '" ' + 'type="radio" value="' + question.choices[counter] + '">' + question.choices[counter] + '<br>';
   return thisAnswer;
 }
 
@@ -57,15 +58,15 @@ function createInputTag(currentQuestion) {
 function showQuestion() {
   var radio = [];
   // create inputs for radio buttons
-  for (counter = 0; counter < questions[currentQuestion].qChoices.length; counter++) {
-    createInputTag(currentQuestion);
-    radio.push(createInputTag(currentQuestion));
-    //console.log(radio);
+  for (counter = 0; counter < question.choices.length; counter++) {
+    createInputTag(question);
+    radio.push(createInputTag(question));
   }
+
   // add question number to .questionNum
-  $(".questionNum").append("<p>" + "Question " + questions[currentQuestion].qNum + " of " + questions.length + "</p>");
+  $(".questionNum").append("<p>" + "Question " + question.num + " of " + questions.length + "</p>");
   // show question text within .questions
-  $(".question").append(questions[currentQuestion].qText);
+  $(".question").append(currentText);
   // add radio buttons to .options 
   $(".options").append(radio);
   // add submit button
@@ -76,35 +77,29 @@ function showQuestion() {
 function gradeQuestion() {
   // get the value of the checked radio button:
   var answerValue = $("input[type='radio']:checked").val();
-  // test value of checked button
-  // console.log(answerValue);
   // hide previous question 
   clearPrevious();
   // compare selected option to answer
-  // test value of current question answer
-  // console.log(questions[currentQuestion].qAnswer);
   if (answerValue == null) {
     alert("Please select an answer.");
     showQuestion();
   }
   else {
   	// show if answer was correct
-  	if (answerValue == questions[currentQuestion].qAnswer) {
+  	if (answerValue == question.answer) {
       // display "correct" message
       $(".question").append("<p>" + "Correct!" + "</p>");
       // add to the score
       score++;
-      // test score
-      // console.log(score);
     }
     else {
       // display "incorrect" message
       $(".question").append("<p>" + "Sorry, that is incorrect." + "</p>");
     }
     // display explanation of correct answer
-    $(".question").append(questions[currentQuestion].qDescription);
+    $(".question").append(question.description);
     // display next question button or score button
-    if (questions[currentQuestion].qNum == 5) {
+    if (question.num == 5) {
       $(".options").append('<input name="score" type="submit" value="Show Score">');
     }
     else {
@@ -137,6 +132,10 @@ $(document).ready(function() {
   $(document).on("click", "input[name='next']", function() {
     // update currentQuestion to next question
     currentQuestion++;
+    // update question variable
+    question = questions[currentQuestion];
+    // update currentText variable
+    currentText = question.text;
     // hide previous question 
     clearPrevious();
     //show question
